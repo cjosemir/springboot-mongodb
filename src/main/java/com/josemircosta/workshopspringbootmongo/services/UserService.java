a1b3a97a4504d016b7ca2591b86573e32b271cba
Josemir Costa <josemirpe@gmail.com>
2020-09-07 15:59:16 -0300

    Inserção de usuário com POST

--- src/main/java/com/josemircosta/workshopspringbootmongo/services/UserService.java
+++ src/main/java/com/josemircosta/workshopspringbootmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.josemircosta.workshopspringbootmongo.domain.User;
+import com.josemircosta.workshopspringbootmongo.dto.UserDTO;
 import com.josemircosta.workshopspringbootmongo.repository.UserRepository;
 import com.josemircosta.workshopspringbootmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 }
