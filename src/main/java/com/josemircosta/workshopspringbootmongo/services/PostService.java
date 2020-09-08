d0985848f3de8626092b3c0ab16fab28df688f3c
Josemir Costa <josemirpe@gmail.com>
2020-09-08 18:46:51 -0300

    Acrescentando comentários aos posts

---
+++ src/main/java/com/josemircosta/workshopspringbootmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.josemircosta.workshopspringbootmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.josemircosta.workshopspringbootmongo.domain.Post;
+import com.josemircosta.workshopspringbootmongo.repository.PostRepository;
+import com.josemircosta.workshopspringbootmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
